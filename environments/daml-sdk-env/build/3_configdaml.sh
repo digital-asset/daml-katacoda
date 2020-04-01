3d168a69d634d66ea6366f771aa41919fe0b786a
Gabriela S. Soria <soria.gaby@gmail.com>
2020-04-01 07:37:54 -0700

    Updated configdaml script
    Signed-off-by: Gabriela S. Soria <soria.gaby@gmail.com>

--- environments/daml-sdk-env/build/3_configdaml.sh
+++ environments/daml-sdk-env/build/3_configdaml.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
-cat <<EOF > /opt/configure-environment.sh
-#!/bin/bash
 echo "PATH=\$PATH:~/.daml/bin" >> ~/.bashrc
 source ~/.bashrc
 
-# uninstall unnecessary extensions
+# uninstall unnecessary VSCode extensions
 rm -rf /opt/.katacodacode/extensions/redhat.vscode-openshift-connector-0.0.19/;
 rm -rf /tmp/.katacodacode/extensions/redhat.vscode-openshift-connector-0.0.19/;
 
-# install daml extension
+# install daml VSCode extension
 cd /tmp && wget http://assets.joinscrapbook.com/daml/DigitalAssetHoldingsLLC.daml-0.13.55.vsix;
 mv DigitalAssetHoldingsLLC.daml-0.13.55.vsix daml-0.13.55.zip;
 mkdir daml-0.13.55 && unzip daml-0.13.55.zip -d daml-0.13.55;
 mv daml-0.13.55/extension /opt/.katacodacode/extensions/daml-0.13.55;
 rm /tmp/daml-0.13.55.zip;
 rm -Rf /tmp/daml-0.13.55;
-EOF
-
-chmod +x /opt/configure-environment.sh
-/opt/configure-environment.sh
-rm /opt/configure-environment.sh
