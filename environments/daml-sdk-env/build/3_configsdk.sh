ab9132c012da200217c24bd5599e64775966e1a6
Bernhard Elsner <bernhard.elsner@digitalasset.com>
2020-04-02 10:39:45 +0200

    Fix issue finding the bundled extension

--- environments/daml-sdk-env/build/3_configsdk.sh
+++ environments/daml-sdk-env/build/3_configsdk.sh
@@ -8,6 +8,6 @@ rm -rf /opt/.katacodacode/extensions/redhat.vscode-openshift-connector-0.0.19
 
 # install the daml extension (the hard way)
 cd /tmp & mkdir daml-bundled
-unzip $(find ~/.daml/sdk -d 1)/studio/daml-bundled.vsix -d daml-bundled
+unzip $(find ~/.daml/sdk/*/studio/daml-bundled.vsix) -d daml-bundled
 mv daml-bundled/extension /opt/.katacodacode/extensions/daml-bundled
 rm -Rf /tmp/daml-bundled
