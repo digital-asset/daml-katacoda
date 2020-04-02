6cef6c8af704a816863109e26b2c78bb3a4f7313
Bernhard Elsner <bernhard.elsner@digitalasset.com>
2020-04-02 10:27:43 +0200

    Fix error in script

--- environments/daml-sdk-env/build/2_installsdk.sh
+++ environments/daml-sdk-env/build/2_installsdk.sh
@@ -2,4 +2,4 @@
 export SDK_VERSION=0.13.55
 
 curl https://get.daml.com | sh -s $SDK_VERSION \
-    && printf "auto-install: false\nupdate-check: never\n" >> /home/daml/.daml/daml-config.yaml
+    && printf "auto-install: false\nupdate-check: never\n" >> $HOME/.daml/daml-config.yaml
