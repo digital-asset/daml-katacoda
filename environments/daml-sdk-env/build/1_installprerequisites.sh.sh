2507a6eafe11a41f5ab9a5c9bed4a37897338c2c
Bernhard Elsner <bernhard.elsner@digitalasset.com>
2020-04-02 10:23:21 +0200

    Switch to adoptopenjdk

--- environments/daml-sdk-env/build/1_installprerequisites.sh.sh
+++ environments/daml-sdk-env/build/1_installprerequisites.sh.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Following https://docs.azul.com/zulu/zuludocs/ZuluUserGuide/PrepareZuluPlatform/AttachAPTRepositoryUbuntuOrDebianSys.htm
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xB1998361219BD9C9
-sudo apt-add-repository 'deb http://repos.azulsystems.com/ubuntu stable main'
+# Following https://adoptopenjdk.net/installation.html
+wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
+sudo add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
 
 # Following https://classic.yarnpkg.com/en/docs/install/#debian-stable
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
@@ -12,4 +12,4 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/source
 curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
 
 sudo apt-get update
-sudo apt install zulu-11 nodejs yarn
+sudo apt install adoptopenjdk-11-hotspot-jre nodejs yarn
