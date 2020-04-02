1bde28441fcaa91d6b6f1a83f523266ce4071925
Bernhard Elsner <bernhard.elsner@digitalasset.com>
2020-04-02 11:38:19 +0200

    Add some important -ys

--- environments/daml-sdk-env/build/1_installprerequisites.sh.sh
+++ environments/daml-sdk-env/build/1_installprerequisites.sh.sh
@@ -13,8 +13,8 @@ curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
 
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install gcc-4.9
-sudo apt-get upgrade libstdc++6
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y gcc-4.9
+sudo apt-get upgrade -y libstdc++6
 sudo apt install adoptopenjdk-11-hotspot-jre nodejs yarn
