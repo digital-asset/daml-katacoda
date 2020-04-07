68ddb5890232a7175d5edbd15a7b594924368f65
Bernhard Elsner <bernhard.elsner@digitalasset.com>
2020-04-07 17:10:01 +0200

    Adding some important backslashes

--- environments/daml-sdk-env/build/2_installsdk.sh
+++ environments/daml-sdk-env/build/2_installsdk.sh
@@ -5,9 +5,9 @@ curl https://get.daml.com | sh -s $SDK_VERSION \
     && printf "auto-install: false\nupdate-check: never\n" >> $HOME/.daml/daml-config.yaml
 
 # Hack to not show the release notes
-mkdir -p opt/.katacodacode/user-data/User/state/
+mkdir -p opt/.katacodacode/user-data/User/state/ \
     && echo '[["DigitalAssetHoldingsLLC.daml-bundled", "{\"version\":\"0.13.55\"}"]]' > /opt/.katacodacode/user-data/User/state/global.json
 
 # Opt out of telemetry
-mkdir ~/.daml
+mkdir ~/.daml \
     && touch ~/.daml/.opted_out
