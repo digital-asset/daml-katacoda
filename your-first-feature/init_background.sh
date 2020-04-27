0e43f3e2cdc42fba31dbfdf0ec259c3edfc400aa
Nemanja Memarovic <nemanja.memarovic@digitalasset.com>
2020-04-27 13:41:26 +0200

    Fix

--- your-first-feature/init_background.sh
+++ your-first-feature/init_background.sh
@@ -6,10 +6,10 @@ init()
     ~/.daml/bin/daml new create-daml-app create-daml-app
     mkdir -p /root/create-daml-app
     mv create-daml-app/* /root/create-daml-app/
-    ls
     cd /root/create-daml-app/
     ls
-    cd ui/src/components
+    cd ui/
+    ls
     touch MessageList.tsx
     touch MessageEdit.tsx
     ls
