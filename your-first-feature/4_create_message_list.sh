6a764929e23759f1ad560dbafee037ee02695c3f
Nemanja Memarovic <nemanja.memarovic@digitalasset.com>
2020-04-27 14:23:24 +0200

    Fix

---
+++ your-first-feature/4_create_message_list.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /root/create-daml-app/
+echo "About to see what's in create-daml-app"
+ls
+cd ui/
+echo "About to see what's in create-daml-app/ui"
+ls
+cd src/components
+echo "About to see what's in create-daml-app/ui/src/components"
+touch MessageList.tsx
+ls
