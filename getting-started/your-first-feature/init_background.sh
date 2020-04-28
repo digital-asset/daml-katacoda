4a117959c7efbd3b6d5cd0f5d8afcd1b0bd91f44
Nemanja Memarovic <nemanja.memarovic@digitalasset.com>
2020-04-28 13:31:21 +0200

    Fix

--- getting-started/your-first-feature/init_background.sh
+++ getting-started/your-first-feature/init_background.sh
@@ -13,7 +13,8 @@ init()
     echo "DANGEROUSLY_DISABLE_HOST_CHECK=true" >> .env
     sed -i 's+ws://localhost:7575/+wss://[[HOST_SUBDOMAIN]]-7575-[[KATACODA_HOST]].environments.katacoda.com/+g' src/config.ts
     cd /root/create-daml-app/daml
-    echo "\n\n" >> User.daml
+    echo "" >> User.daml
+    echo "" >> User.daml
 }
 
 echo Initialising...
