bdc6753b178a667758e323eaa1703bd30c1f16fb
Nemanja Memarovic <nemanja.memarovic@digitalasset.com>
2020-04-28 09:46:45 +0200

    Fix

--- your-first-feature/init_background.sh
+++ your-first-feature/init_background.sh
@@ -6,9 +6,12 @@ init()
     ~/.daml/bin/daml new create-daml-app create-daml-app
     mkdir -p /root/create-daml-app
     mv create-daml-app/* /root/create-daml-app/
+    cd /root/create-daml-app/
+    echo "json-api-options:" >> daml.yaml
+    evhi "- --address=0.0.0.0" >> daml.yaml
     cd /root/create-daml-app/ui
     echo "DANGEROUSLY_DISABLE_HOST_CHECK=true" >> .env
-    sed -i 's+ws://localhost:7575/+wss://2886795313-7575-maple01.environments.katacoda.com/+g' src/config.ts
+    sed -i 's+ws://localhost:7575/+wss://2886795313 -7575-maple01.environments.katacoda.com/+g' src/config.ts
 }
 
 echo Initialising...
