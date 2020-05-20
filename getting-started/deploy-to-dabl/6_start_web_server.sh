f06177e9caf1ba1d1e31c9a93a8d410131f78e91
Dimitri Liakakos <dimitri.liakakos@digitalasset.com>
2020-05-20 16:11:56 -0400

    modify background script for step 6

--- getting-started/deploy-to-dabl/6_start_web_server.sh
+++ getting-started/deploy-to-dabl/6_start_web_server.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-init()
-{
-    cd /root/create-daml-app/target
-    /usr/bin/python2 -m SimpleHTTPServer 8080 &
-}
-
-echo Initialising...
-init
-echo Done!
+cd /root/create-daml-app/target
+/usr/bin/python2 -m SimpleHTTPServer 8080 &
