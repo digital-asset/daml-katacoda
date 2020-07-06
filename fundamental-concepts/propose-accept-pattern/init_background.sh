a73483675426fff9905d02edede8ab35ee0118b9
robin@digitalasset.com <drsk@lavabit.com>
2020-07-06 17:27:01 +0200

    fix the shell script path in the propose/accept scenario.
    CHANGELOG_BEGIN
    CHANGELOG_END

--- fundamental-concepts/propose-accept-pattern/init_background.sh
+++ fundamental-concepts/propose-accept-pattern/init_background.sh
@@ -2,7 +2,7 @@
 
 echo initializing...
 
-wget https://katacoda.com/drsk/courses/fundamental-concepts/propose-accept-pattern/assets/create-daml-app.tar.gz
+wget https://katacoda.com/daml/courses/fundamental-concepts/propose-accept-pattern/assets/create-daml-app.tar.gz
 
 daml new create-daml-app empty-skeleton
 
