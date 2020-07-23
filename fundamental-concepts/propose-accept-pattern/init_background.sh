39df04ea6db939fc5e5d95af779af36e4b77d67e
Nemanja Memarovic <nemanja.memarovic@digitalasset.com>
2020-07-23 16:31:14 +0200

    Reverting propose/accept to previous state

--- fundamental-concepts/propose-accept-pattern/init_background.sh
+++ fundamental-concepts/propose-accept-pattern/init_background.sh
@@ -8,10 +8,6 @@ daml new create-daml-app empty-skeleton
 
 tar xzf create-daml-app.tar.gz
 
-sdk_version=$(~/.daml/bin/daml version | awk '/(default SDK version for new projects)/ {print $1}')
-echo "DAML SDK version is" $sdk_version
-sed -i "/sdk-version: /c\sdk-version: $sdk_version" /root/create-daml-app/daml.yaml
-
 cd create-daml-app
 
 echo done
