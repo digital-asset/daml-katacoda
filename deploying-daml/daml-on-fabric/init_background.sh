efe0e608eeffd77831bde94d3cabe8a96793ea04
Anthony Lusardi <anthony.lusardi@digitalasset.com>
2021-06-22 08:36:52 -0500

    fix sdk mismatch another way

--- deploying-daml/daml-on-fabric/init_background.sh
+++ deploying-daml/daml-on-fabric/init_background.sh
@@ -6,8 +6,6 @@ init()
     rm -rf $HOME/my-app
     rm -rf $HOME/daml-on-fabric
     rm -rf $HOME/fabric
-    daml install --install-assistant=yes 1.10.0
-    echo "export DAML_SDK_VERSION=1.10.0" >> $HOME/.bashrc
 }
 
 echo Initialising...
