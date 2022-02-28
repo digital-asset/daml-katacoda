1a2715696f2c1588becf9a243f57105b7f80b939
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2022-02-28 22:15:54 +0100

    Fix upgrading scenario: part 3

--- upgrading/upgrade-ui/init_background.sh
+++ upgrading/upgrade-ui/init_background.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
+setup_vs_extension()
+{
+    ~/.daml/bin/daml install 2.0.0-snapshot.20220225.9368.0.b291a57e
+    rm -rf daml-bundled
+    rm -rf /opt/.katacodacode/extensions/daml-bundled
+    echo "Removed the VS"
+    curl -sSL https://gist.githubusercontent.com/cocreature/b616b7ff1244b227ee17cdc476695435/raw/fd337aab4460e2e81074bc3dfd3e527b52c233d5/patch-extension.sh > patch-extension.sh
+    echo "Got the extension"
+    chmod +x patch-extension.sh
+    ./patch-extension.sh $(find ~/.daml/sdk/2.*/studio/daml-bundled.vsix) patched-daml-bundled.vsix
+    echo "Patched it"
+    unzip patched-daml-bundled.vsix -d daml-bundled
+    echo "unzipped it"
+    mv daml-bundled/extension /opt/.katacodacode/extensions/daml-bundled
+    echo "moved it"
+    rm -rf daml-bundled
+    rm patched-daml-bundled.vsix
+    rm patch-extension.sh
+}
 
 echo initializing...
+setup_vs_extension
 
 # workaround for assets in the `assets` directory, because google-chrome complains when the file
 # size is bigger than 50kb
