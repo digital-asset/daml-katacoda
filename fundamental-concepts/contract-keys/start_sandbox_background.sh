b0f83ce69580775296489e634af16e519a73b260
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2022-03-01 16:15:19 +0100

    Fix contract keys scenario
    I didn’t try to fix the jwt binary yet. Let’s first fix that for the
    other scenario an dthen translate it to this.

--- fundamental-concepts/contract-keys/start_sandbox_background.sh
+++ fundamental-concepts/contract-keys/start_sandbox_background.sh
@@ -5,16 +5,13 @@ echo Starting sandbox
 rm -rf create-daml-app
 daml create-daml-app create-daml-app
 cd create-daml-app
-sed -i "13i - daml-script" daml.yaml
-sed -i "6i import Daml.Script" ./daml/User.daml
+sed -i "6i import Daml.Script hiding (User)" ./daml/User.daml
 sed -i "7i import DA.Optional" ./daml/User.daml
-sed -i "8i \n" ./daml/User.daml
-
+sed -i '8i \
+' ./daml/User.daml
 daml build
 
-daml sandbox --ledgerid MyLedger &
-sleep 5
+daml sandbox --dar .daml/dist/create-daml-app-0.1.0.dar &
 daml json-api --ledger-host=localhost --ledger-port=6865 --http-port=7575 --address=0.0.0.0 --allow-insecure-tokens &
-daml ledger upload-dar .daml/dist/create-daml-app-0.1.0.dar
 
 echo Done!
