ffa999d074910a2c39d4edea46716cfd7ae0d146
Nemanja Memarovic <nemanja.memarovic@digitalasset.com>
2022-03-04 15:28:43 +0100

    Adding the migration dar upload to the start script

--- upgrading/upgrade-ui/start_sandbox_background.sh
+++ upgrading/upgrade-ui/start_sandbox_background.sh
@@ -6,6 +6,7 @@ daml ledger upload-dar create-daml-app-0.1.0.dar
 daml ledger upload-dar create-daml-app-0.1.1.dar
 daml ledger upload-dar forum-0.1.0.dar
 daml ledger upload-dar forum-0.1.1.dar
+daml ledger upload-dar migration-0.1.0.dar
 daml script --dar forum-0.1.0.dar --script-name Init:initialize --ledger-host localhost --ledger-port 6865 --wall-clock-time
 daml script --dar forum-0.1.1.dar --script-name Init:initialize --ledger-host localhost --ledger-port 6865 --wall-clock-time
 daml json-api --ledger-host=localhost --ledger-port=6865 --http-port=7575 --address=0.0.0.0 --allow-insecure-tokens &
